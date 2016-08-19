1ddb4ab681329835565a0f9123ff2629dd24a543
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-08-19 09:50:55 -0700

    Send an error message if user enters zero and pong if a number is divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,41 +3,31 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = $("input").val();
-    // var result = pingPong(userInput);
-    // $("#output").text(result);
     pingPong(userInput);
   });
 });
-
 // business logic
 function pingPong(userInput){
-  // debugger;
   $("#output").empty();
-  for (var i = 1; userInput >= i; i++){
-    if (i % 15 === 0){
-      $("#output").append("<li>" + "ping-pong!" + "</li>");
-    }
-    else if (i % 3 === 0){
-      $("#output").append("<li>" + "ping" + "</li>");
-    }
-    else if (i % 5 === 0){
-      $("#output").append("<li>" + "pong" + "</li>");
-    }
-    else {
-    $("#output").append("<li>" + i + "</li>");
-    // console.log(i);
+  if (userInput === "0"){
+    $("#output").append("No zeroes please!");
+  }
+  else {
+    for (var i = 1; userInput >= i; i++){
+      if (i % 15 === 0){
+        $("#output").append("<li>" + "ping-pong!" + "</li>");
+      }
+      else if (i % 3 === 0){
+        $("#output").append("<li>" + "ping" + "</li>");
+      }
+      else if (i % 5 === 0){
+        $("#output").append("<li>" + "pong" + "</li>");
+      }
+      else {
+      $("#output").append("<li>" + i + "</li>");
+      // console.log(i);
+      }
     }
   }
   return (i);
 }
-
-
-// function pingPong(userInput){
-//   var numberList = "";
-//   $("#output").empty();
-//   for (var i = 1; userInput >= i; i++){
-//     $("#output").append("<li>" + i + "</li>");
-//     console.log(i);
-//   }
-//   return (i);
-// }
