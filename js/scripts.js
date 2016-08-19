69eb514c6a21dbd6e0e544a86bf523c1bbca262b
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-08-19 09:24:27 -0700

    Add js to print a list of numbers up to user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,36 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("input").val());
-    // console.log(userInput);
-    var result = pingPong(userInput);
-     $("#output").text(result);
-
+    var userInput = $("input").val();
+    // var result = pingPong(userInput);
+    // $("#output").text(result);
+    pingPong(userInput);
   });
 });
+
 // business logic
 function pingPong(userInput){
+  // debugger;
+  $("#output").empty();
   for (var i = 1; userInput >= i; i++){
+    if (i === 3){
+      $("#output").append("ping");
+    }
+    else {
+    $("#output").append("<li>" + i + "</li>");
     console.log(i);
+    }
   }
+  return (i);
 }
+
+
+// function pingPong(userInput){
+//   var numberList = "";
+//   $("#output").empty();
+//   for (var i = 1; userInput >= i; i++){
+//     $("#output").append("<li>" + i + "</li>");
+//     console.log(i);
+//   }
+//   return (i);
+// }
