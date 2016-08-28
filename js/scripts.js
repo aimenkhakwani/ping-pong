e2a33a62ce8c8e2a57d0120bb3c634f0f69f5a3b
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-08-27 19:37:06 -0700

    Seperate ui and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,40 @@
+// business logic
+function validate(userInput){
+  if(userInput < 1){
+    return("No zeroes please!");
+  }
+  else {
+     return pingPong(userInput);
+  }
+}
+function pingPong(userInput){
+  var result = [];
+  for (var i = 1; userInput >= i; i++){
+    if (i % 15 === 0){
+      result.push("ping-pong!");
+    }
+    else if (i % 3 === 0){
+      result.push("ping");
+    }
+    else if (i % 5 === 0){
+      result.push("pong");
+    }
+    else {
+      result.push(i);
+    }
+  }
+  return result;
+}
 // ui logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    $("#output").empty();
     var userInput = parseInt($("input").val());
-    $("#output").text(pingPong(userInput));
+    var output = validate(userInput);
+
+    output.forEach(function(item){
+    $("#output").append("<li>" + item + "</li>");
+    });
   });
 });
-// business logic
-function pingPong(userInput){
-  $("#output").empty();
-  if (userInput === 0){
-    $("#output").append("No zeroes please!");
-  }
-  else {
-    for (var i = 1; userInput >= i; i++){
-      if (i % 15 === 0){
-        $("#output").append("<li>" + "ping-pong!" + "</li>");
-      }
-      else if (i % 3 === 0){
-        $("#output").append("<li>" + "ping" + "</li>");
-      }
-      else if (i % 5 === 0){
-        $("#output").append("<li>" + "pong" + "</li>");
-      }
-      else {
-      $("#output").append("<li>" + i + "</li>");
-      }
-    }
-  }
-}
